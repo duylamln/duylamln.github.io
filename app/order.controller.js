aeaf5c305f0788c7f8a24a69040cab6604d2b0ae
Lam Le <duylamln@gmail.com>
2018-12-29 21:02:16 +0700

    add button copy order detail link

--- app/order.controller.js
+++ app/order.controller.js
@@ -1,7 +1,7 @@
 ﻿(function (module) {
     module.controller("OrderController", orderController);
-    orderController.$inject = ["$sce", "$scope", "$timeout", "$state", "orderService"];
-    function orderController($sce, $scope, $timeout, $state, orderService) {
+    orderController.$inject = ["$sce", "$scope", "$timeout", "$state", "orderService","Alertify"];
+    function orderController($sce, $scope, $timeout, $state, orderService, Alertify) {
         var model = this;
         model.createNewOrder = createNewOrder;
         model.selectOrder = selectOrder;
@@ -9,6 +9,8 @@
         model.goToOrderDetail = goToOrderDetail;
         model.lockOrder = lockOrder;
         model.unlockOrder = unlockOrder;
+        model.calculateOrderPrice = calculateOrderPrice;
+        model.copyOrderDetailLink = copyOrderDetailLink;
 
         activate();
 
@@ -23,7 +25,12 @@
                     model.orders = _.orderBy(data, "date", "desc");
 
                     if (model.selectedOrder) {
-                        model.selectedOrder = _.find(model.activeOrders, { key: model.selectedOrder.key });
+                        model.selectedOrder = _.find(model.orders, { key: model.selectedOrder.key });
+                    }
+                    else {
+                        if (model.orders && model.orders.length > 0) {
+                            model.selectedOrder = model.orders[0];
+                        }
                     }
                 })
             });
@@ -42,7 +49,7 @@
 
         function selectOrder(order) {
             model.selectedOrder = order;
-        }       
+        }
 
         function removeOrder(order, $event) {
             orderService.removeOrder(order);
@@ -51,20 +58,24 @@
             $event.preventDefault();
         }
 
-        function lockOrder(order, $event){
+        function lockOrder(order, $event) {
             order.status = "locked";
 
-            orderService.updateOrder(order);
+            orderService.updateOrder(order).then(function(){
+                Alertify.success("Order " + order.key + " locked");
+            });
 
             $event.stopPropagation();
             $event.preventDefault();
         }
 
-        
-        function unlockOrder(order, $event){
+
+        function unlockOrder(order, $event) {
             order.status = "active";
 
-            orderService.updateOrder(order);
+            orderService.updateOrder(order).then(function(){
+                Alertify.success("Order " + order.key + " activated");
+            });
 
             $event.stopPropagation();
             $event.preventDefault();
@@ -73,6 +84,22 @@
         function goToOrderDetail() {
             if (!model.selectedOrder) return;
             return $state.go("main.orderDetail", { key: model.selectedOrder.key });
+        }
+
+        function calculateOrderPrice() {
+            return _.reduce(model.selectedOrder.detail, function (sum, item) {
+                if (isNaN(Number.parseFloat(item.price))) return sum += 0;
+                return sum += Number.parseFloat(item.price);
+            }, 0);
+        }
+
+        function copyOrderDetailLink(order, $event){
+            var orderDetailHref = location.href + "/" + order.key;
+            copyToClipboard(orderDetailHref);
+            Alertify.success("Copied: "+ orderDetailHref);
+
+            $event.stopPropagation();
+            $event.preventDefault();
         }
 
     }
